9d32381ef43aba967d40f2a1907f3491957651c1
Alejandro Lopez <alejandrolpz0619@gmail.com>
2024-01-24 13:24:44 -0400

    Update Colores.java

--- java-swing-project/src/proyecto/Colores.java
+++ java-swing-project/src/proyecto/Colores.java
@@ -3,7 +3,7 @@ import java.awt.*;
 
 /**
  *
- * @author juan
+ * @author juan y pastaExplosivaBooom
  */
 public class Colores {
 
@@ -13,4 +13,12 @@ static Color lightBlue = new Color(211,222,255,255);
 static Color darkYellow = new Color(179,128,54,255);
 static Color lightYellow = new Color(255,184,77,255);
 
+static Color turquoise = new Color(143, 192, 169, 255);
+static Color almond = new Color(239, 222, 205, 255);
+static Color beaver = new Color (159, 129, 112, 255);
+static Color liver = new Color(83, 75, 79, 255);
+static Color brown = new Color (115, 87, 81, 255);
+
+
 }
+
